6d9021cdc52d578ea2d1b7ed33ecd5a60aa269b9
iFeYZenX <flo91680@gmail.com>
2016-12-12 23:00:48 +0100

    Creation classe pour exporter un fichier avec la liste des vols

---
+++ src/config/EcritureLectureFichier.java
@@ -0,0 +1,61 @@
+package config;
+import Entities.Utilisateur;
+import Entities.Vol;
+import Services.VolService;
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Created by Florian on 12/12/2016.
+ */
+public class EcritureLectureFichier {
+
+    private String nomFichier;
+    private VolService volService = new VolService();
+    private Utilisateur utilisateurCourant;
+
+    public EcritureLectureFichier(String nomFichier, Utilisateur utilisateur){
+
+        this.nomFichier = nomFichier;
+        this.utilisateurCourant = utilisateur;
+
+        try {
+            FileOutputStream fos = new FileOutputStream(this.nomFichier);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            ArrayList<Vol> vols = this.volService.findAllByMembreEquipage(this.utilisateurCourant.getIdMembre());
+
+            //si la liste de vol n'est pas nule on ajoute tous les vols dans le fichier
+            if(vols != null){
+                for(Vol vol: vols) {
+                    //on récupère les données du vols
+                    String numero = "Numéro de vol : " + vol.getNumero();
+                    String nomSite = ", Nom site : " + vol.getSite().getNom();
+                    String nomDestination = ", Nom destination : " + vol.getDestination().getNom();
+                    String date = ", Date : " + vol.getDate();
+                    String refAvion = ", Ref avion : " + vol.getAvion().getRef();
+                    String pilote =  ", Pilote : " + vol.getEquipage().getPilote().getNom();
+                    String copilote = ", Copilote : " + vol.getEquipage().getCopilote().getNom() + ".";
+                    //on concatène les strings
+                    Object[] objs = {numero, nomSite, nomDestination, date, refAvion, pilote, copilote};
+                    //on ajoute une ligne dans le fichier
+                    oos.writeObject(objs);
+                    //on vide le tampon
+                    oos.flush();
+                }
+            }
+
+            //on termine
+            oos.close();
+
+
+        }
+        catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNomFichier() {
+        return nomFichier;
+    }
+}
